cf53f6423f4d1210353b3e81b35bf14e1abb45a7
d.takeda <dt.230801@gmail.com>
2026-03-17 09:57:50 +0900

    Working...

--- src/fw/WorldNavigator.h
+++ src/fw/WorldNavigator.h
@@ -4,12 +4,26 @@
 
 class WorldNavigator {
 public:
+	typedef struct {
+		unsigned int mXMin;
+		unsigned int mYMin;
+		unsigned int mZMin;
+		unsigned int mXMax;
+		unsigned int mYMax;
+		unsigned int mZMax;
+		unsigned int mRate;
+	} SpaceConfig;
+
 	//////////////////////////////////////////////////////////
 	// APIs
 	//////////////////////////////////////////////////////////
 	static void createInstance();
 	static void destroyInstance();
 	static WorldNavigator& getInstance();
+
+	unsigned int createWorld(SpaceConfig& space);
+	unsigned int getCurrentWorld();
+	int			 changeWorld(unsigned int id);
 
 	virtual ~WorldNavigator() = default;
 private:

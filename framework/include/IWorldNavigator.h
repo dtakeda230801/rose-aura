c159d3f6e0860f55816324ada2674f935ec265de
d.takeda <dt.230801@gmail.com>
2026-03-19 23:45:29 +0900

    start implementation for world navigator

--- framework/include/IWorldNavigator.h
+++ framework/include/IWorldNavigator.h
@@ -3,19 +3,41 @@
 class IWorldNavigator {
 public:
 	typedef struct {
-		unsigned int mXMin;
-		unsigned int mYMin;
-		unsigned int mZMin;
-		unsigned int mXMax;
-		unsigned int mYMax;
-		unsigned int mZMax;
+		int mXMin;
+		int mYMin;
+		int mZMin;
+		int mXMax;
+		int mYMax;
+		int mZMax;
 		unsigned int mRate;
 	} SpaceConfig;
+
+	typedef struct {
+		int mXMin;
+		int mYMin;
+		int mZMin;
+		int mXMax;
+		int mYMax;
+		int mZMax;
+	} ActiveSpace;
+
+	typedef struct {
+		int mX;
+		int mY;
+		int mZ;
+	} Location;
+
+	typedef unsigned int WORLD_ID;
+
+	#define isValidWorldId(x) 0!=x 
 
 	//////////////////////////////////////////////////////////
 	// APIs
 	//////////////////////////////////////////////////////////
-	virtual unsigned int createWorld(SpaceConfig& space) = 0;
-	virtual unsigned int getCurrentWorld()				 = 0;
-	virtual int			 changeWorld(unsigned int id)	 = 0;
+	virtual WORLD_ID	 createWorld(SpaceConfig& space)    = 0;
+	virtual WORLD_ID	 getCurrentWorld()				    = 0;
+	virtual void		 setCurrentWorld(WORLD_ID id)       = 0;
+	virtual WORLD_ID	 changeWorld(WORLD_ID id)	        = 0;
+	virtual ActiveSpace& getActiveSpace()					= 0;
+	virtual void		 setActiveSpace(ActiveSpace& space) = 0;
 };

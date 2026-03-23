abab96548899a640a1ebe3dc1e41f42fec913e80
d.takeda <dt.230801@gmail.com>
2026-03-23 23:19:59 +0900

    consider APIs of World Navigator

--- framework/include/IWorldNavigator.h
+++ framework/include/IWorldNavigator.h
@@ -2,25 +2,43 @@
 
 class IWorldNavigator {
 public:
-	typedef struct {
+	struct Vec3 {
 		int mX;
 		int mY;
 		int mZ;
-	} Vec3;
+	};
 
-	typedef struct {
+	struct ActiveSpace {
 		Vec3 mMin;
 		Vec3 mMax;
-		Vec3 mNonScrollRange;
-		unsigned int mRate;
+	};
+
+	typedef struct {
+		Vec3		mMin;
+		Vec3		mMax;
+		Vec3		mNonScrollRange;
+		ActiveSpace mActiveSpace;
 	} SpaceConfig;
 
-	typedef struct {
-		Vec3 mMin;
-		Vec3 mMax;
-	} ActiveSpace;
+	using WORLD_ID = unsigned int;
+	using EVENT_ID = unsigned int;
 
-	typedef unsigned int WORLD_ID;
+	class IApproachingCallback {
+	public:
+		virtual bool checkApproaching(WORLD_ID worldId, EVENT_ID eventId, Vec3& position) = 0;
+
+		IApproachingCallback() = default;
+		virtual ~IApproachingCallback() = default;
+	};
+
+	class IEventCallback {
+	public:
+		virtual void onEvent(WORLD_ID worldId, EVENT_ID eventId) = 0;
+
+		IEventCallback() = default;
+		virtual ~IEventCallback() = default;
+	};
+
 
 	#define isValidWorldId(x) 0!=x 
 
@@ -29,8 +47,16 @@ public:
 	//////////////////////////////////////////////////////////
 	virtual WORLD_ID	 createWorld(SpaceConfig& space)    = 0;
 	virtual WORLD_ID	 getCurrentWorld()				    = 0;
-	virtual void		 setCurrentWorld(WORLD_ID id)       = 0;
+	virtual int 		 setCurrentWorld(WORLD_ID id)       = 0;
 	virtual WORLD_ID	 changeWorld(WORLD_ID id)	        = 0;
 	virtual ActiveSpace& getActiveSpace()					= 0;
-	virtual void		 setActiveSpace(ActiveSpace& space) = 0;
+	virtual int 		 setActiveSpace(ActiveSpace& space) = 0;
+	virtual int          updatePosition(Vec3& center)		= 0;
+	virtual Vec3&		 getPosition()						= 0;
+
+	virtual int			 registerEvent(EVENT_ID ev, Vec3& location, float distance) = 0;
+	virtual int			 removeEvent(EVENT_ID ev)	= 0;
+
+	virtual void		 registerApproachingCallback(IApproachingCallback* approachingCallback) = 0;
+	virtual void		 registerEventCallback() = 0;
 };

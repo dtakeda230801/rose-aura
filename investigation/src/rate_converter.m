5dbc258153ee0de50b8037ecf8b8d10dc35577d3
d.takeda <dt.230801@gmail.com>
2026-04-05 09:52:51 +0900

    update investigation for samplingrate converter

--- investigation/src/rate_converter.m
+++ investigation/src/rate_converter.m
@@ -1,4 +1,4 @@
-function dst = rate_converter(in, src_period, dst_period)
+function dst = rate_converter(src, src_period, dst_period)
 
   persistent dst_point
   persistent src_point
@@ -13,24 +13,23 @@ function dst = rate_converter(in, src_period, dst_period)
   endif
 
   if isempty(src_final_sample)
-    src_final_sample = [];
+    src_final_sample(1,1) = 0;
   endif
 
-  _in = [ src_final_sample(:); in(:) ];
-  len = length(_in);
+  _src = [ src_final_sample(:); src(:) ];
+  len = length(_src);
 
   dst_count = 1;
   dst = [];
 
-  for i = 1 : len - 2
+  for i = 1 : len - 1
     t_src          = src_point     * src_period;
     t_src_plus_one = (src_point+1) * src_period;
-
 
     while 1
       t_dst = dst_point * dst_period;
       if (t_src <= t_dst && t_dst  < t_src_plus_one)
-        dst(dst_count,1) = (_in(i+1) - _in(i)) * (t_dst - t_src) + _in(i);
+        dst(dst_count,1) = (_src(i+1) - _src(i)) * (t_dst - t_src) + _src(i);
         dst_point = dst_point + 1;
         dst_count = dst_count + 1;
       else
@@ -39,7 +38,5 @@ function dst = rate_converter(in, src_period, dst_period)
     endwhile
     src_point = src_point + 1;
   endfor
-
-  src_point        = src_point - 1;
-  src_final_sample = _in(length(_in));
+  src_final_sample = _src(length(_src));
 end
--- investigation/src/src.m
+++ investigation/src/src.m
@@ -1,15 +1,16 @@
 clear;
 close all;
-pkg load signal
+pkg load signal;
+warning("off");
 
 %%%
-[x, fs] = audioread("test.wav");
+[in, fs] = audioread("test.wav");
 
-if columns(x) == 2
-  x = mean(x, 2);
+if columns(in) == 2
+  in = mean(in, 2);
 endif
 
-N = length(x);
+in_len = length(in);
 
 %%%
 clear rate_converter
@@ -20,25 +21,25 @@ window_size  = 1024;
 window_start = 1;
 window_end   = window_start + window_size - 1;
 
-src_out = [];
+out = [];
 
-while window_start < N
-  y = x(window_start:window_end);
+while window_start < in_len
+  in_with_win = in(window_start:window_end);
 
-  src_out_temp = rate_converter( y, 1/fs , 1 / dst_fs );
-  src_out_len = length(src_out);
-  src_out(src_out_len + 1 : src_out_len + length(src_out_temp),1) = src_out_temp;
+  out_temp = rate_converter( in_with_win, 1/fs , 1 / dst_fs );
+  out_len = length(out);
+  out(out_len + 1 : out_len + length(out_temp),1) = out_temp;
 
   window_start = window_start + window_size;
   window_end   = window_start + window_size - 1;
-  if window_end > N
-    window_end = N;
+  if window_end > in_len
+    window_end = in_len;
   endif
 endwhile
 
-size(src_out)
+printf("sample num %d -> %d\n",in_len,length(out))
 
-custom_plot(x      ,441000)
-custom_plot(src_out,dst_fs)
+custom_plot(in,fs)
+custom_plot(out,dst_fs)
 
 
